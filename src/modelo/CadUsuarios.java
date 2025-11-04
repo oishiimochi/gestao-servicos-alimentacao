3ae7a833db508cd43ea9fb03c5684b0997d53402
Luiz91558 <luiz.hpereira@ufrpe.br>
2025-11-04 00:25:05 -0300

    O codigo de Usuario e o repositorio de fornecedor, e as excessoes de usuario ainda vou fazer o cad de funcionario e suas excessoes

---
+++ src/modelo/CadUsuarios.java
@@ -0,0 +1,35 @@
+package modelo;
+import servico.*;
+import Exeptions.*;
+
+public class CadUsuarios {
+    private RepositorioUsuario repositorio;
+    public CadUsuarios(RepositorioUsuario repositorio) {
+        this.repositorio = repositorio;
+    }
+    public CadUsuarios() {
+        this.repositorio = new RepositorioUsuario();
+    }
+    public void adicionarUsuario(Usuario usuario) throws IDExistenteException {
+        if(repositorio.buscarUsuario(usuario.getId()) == null) {
+            repositorio.adicionarUsuario(usuario);
+        }
+        else {
+            throw new IDExistenteException(usuario.getId());
+        }
+    }
+    public void removerUsuario(Usuario usuario) throws IdNaoEncontradoException{
+        if(repositorio.buscarUsuario(usuario.getId()) == null) {
+            throw new IdNaoEncontradoException(usuario.getId());
+        }
+        repositorio.removerUsuario(usuario);
+    }
+    public Usuario BuscarUsuario(int ID) throws IdNaoEncontradoException {
+        Usuario usuario = repositorio.buscarUsuario(ID);
+        if(usuario == null) {
+            throw new IdNaoEncontradoException(ID);
+        }
+        return usuario;
+    }
+
+}
---
+++ src/servico/RepositorioFornecedor.java
@@ -0,0 +1,30 @@
+package servico;
+import modelo.*;
+import java.util.ArrayList;
+
+import java.util.ArrayList;
+
+public class RepositorioFornecedor {
+    private ArrayList<Fornecedor> repositorio;
+
+    public RepositorioFornecedor() {
+        this.repositorio = new ArrayList<>();
+    }
+    public ArrayList<Fornecedor> getRepositorio() {
+        return repositorio;
+    }
+    public void adicionarFornecedor(Fornecedor fornecedor) {
+        this.repositorio.add(fornecedor);
+    }
+    public void removerFornecedor(Fornecedor fornecedor) {
+        this.repositorio.remove(fornecedor);
+    }
+    public Fornecedor buscarFornecedor(int ID) {
+        for (Fornecedor fornecedor : repositorio) {
+            if (fornecedor.getId() == ID) {
+                return fornecedor;
+            }
+        }
+        return null;
+    }
+}
